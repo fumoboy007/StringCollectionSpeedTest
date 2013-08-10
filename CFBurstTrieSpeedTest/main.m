3530f67a6f16c2c61d63c82b49dc0b7023b3bd4f
Darren Mo <darren.mo@me.com>
2013-08-10 17:30:03 -0400

    Initial Commit

---
+++ CFBurstTrieSpeedTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CFBurstTrieSpeedTest
+//
+//  Created by Darren Mo on 2013-08-10.
+//  Copyright (c) 2013 Darren Mo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
